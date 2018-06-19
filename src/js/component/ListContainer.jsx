51d2cad885842f52ace8fed53c06acba5c12e3b4
Emily Vong <emily.vong25@gmail.com>
2018-06-19 12:46:31 +0000

    layout and styling complete

--- src/js/component/ListContainer.jsx
+++ src/js/component/ListContainer.jsx
@@ -6,17 +6,30 @@ export default class ListContainer extends React.Component{
     
     render(){
         return (
-            <div>
-                <h1>To Dos</h1>
-                <div id="listItemContainer">
-                    <input></input>
-                    <ul>
-                        <li>Item 1</li>
-                        <li>Item 2</li>
-                        <li>Item 3</li>
-                        <li>Item 4</li>
+            <div id="listContainer" className="card mx-auto">
+                <h1 className="card-header text-center">To Dos</h1>
+                <div id="listItemContainer" className="card-body">
+                    <input type="text" className="form-control" placeholder="Add task"></input>
+                    <ul className="list-group">
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            Item 1
+                            <button type="button" className=" close text-muted">&times;</button>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            Item 2
+                            <button type="button" className=" close text-muted">&times;</button>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            Item 3
+                            <button type="button" className=" close text-muted">&times;</button>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            Item 4
+                            <button type="button" className=" close text-muted">&times;</button>
+                        </li>
                     </ul>
                 </div>
+                <p className="card-footer text-muted mb-0">X items left</p>
             </div>
         );
     }
