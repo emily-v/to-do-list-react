c1a864d8f3925da8fd4dd1a4f7329c125b57febe
Emily Vong <emily.vong25@gmail.com>
2018-06-22 14:42:58 +0000

    add items functioning, delete items in progress

--- src/js/component/ListContainer.jsx
+++ src/js/component/ListContainer.jsx
@@ -6,16 +6,13 @@ export default class ListContainer extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            inputValue:'',
             tasks:["Item 1", "Item 2", "Item 3", "Item 4"]
         };
-        
-        //this.AddTask = this.AddTask.bind(this);
     }
     
-    handleEnter() {
-        this.setState({
-            tasks: this.state.tasks + this.props.input
-        });
+    handleChange(event) {
+        this.setState({ inputValue: event.target.value});
     }
     
     handleKeyDown(event) {
@@ -24,23 +21,52 @@ export default class ListContainer extends React.Component{
         }
     }
     
+    handleEnter(event) {
+        this.setState({
+            inputValue: '',
+            tasks: [...this.state.tasks, this.state.inputValue]
+        });
+    }
+    
+    /*handleClickDelete (event) {
+        var index = parseInt(this.props.index);
+        this.deleteTask(this.index);
+    }*/
+    
+    /*deleteTask () {
+        var taskIndex = this.state.tasks.index;
+        this.state.tasks.splice(this.taskIndex,1);
+        console.log(taskIndex);
+        this.setState({tasks: this.state.tasks});
+    }*/
     
     render(){
         return (
             <div id="listContainer" className="card mx-auto">
                 <h1 className="card-header text-center">To Dos</h1>
                 <div id="listItemContainer" className="card-body">
-                    <input type="text" className="form-control" placeholder="Add task" value={this.props.input} onKeyDown={this.props.handleKeyDown}></input>{/*can change event to arrow function and remove binder?*/}
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Add task" 
+                        value={this.state.inputValue} 
+                        onChange={ (event) => this.handleChange(event) } 
+                        onKeyDown={ (event) => this.handleKeyDown(event) }>
+                    </input>
                     <ul className="list-group">
-                        {this.state.tasks.map(function(task, i){
-                            return <li key={i} className="list-group-item d-flex justify-content-between align-items-center">{task}
-                                <button type="button" className="close text-muted">&times;</button>
-                            </li>;
-                            })
+                        {
+                        this.state.tasks.map((task,index) => (<li key={index} className="list-group-item d-flex justify-content-between align-items-center">{task}
+                            <button 
+                                type="button" 
+                                className="close text-muted"
+                                onClick={(event) => (console.log(this.props.index))}>
+                                &times;
+                            </button>
+                        </li>))
                         }
                     </ul>
                 </div>
-                <p className="card-footer text-muted mb-0">X items left</p>
+                <p className="card-footer text-muted mb-0">{this.state.tasks.length} items left</p>
             </div>
         );
     }
@@ -49,20 +75,6 @@ export default class ListContainer extends React.Component{
 ListContainer.propTypes = {
     task: PropTypes.array,
     input: PropTypes.string,
-    handleKeyDown: PropTypes.func
+    index: PropTypes.number,
+    deleteTask: PropTypes.func
 };
-
-/*export class AddTask extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputValue: ""
-        };
-    }trying AddTask function instead of class*/
-    
-/*function AddTask(props){    
-    return(
-        
-    );
-};
-*/
